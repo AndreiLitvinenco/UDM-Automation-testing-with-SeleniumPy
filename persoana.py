b8f5bd3f45c47fae20aee5c79a40a4513dfee339
Andrei <andreialitvinenco@gmail.com>
2023-12-07 19:53:38 +0200

    Added the missing file for the oop homework.

---
+++ persoana.py
@@ -0,0 +1,10 @@
+#   Clasa persoana
+
+class Persoana:
+    def __init__(self, varsta, sex):
+        self.varsta = varsta
+        self.sex = sex
+    def alearga(self):
+        print("Pot sa alerg")
+    def mananca(self):
+        print("Pot sa alerg")

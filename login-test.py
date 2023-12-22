afe6ad71cf2a225820aa85cb5f8ecfbdebf2cb79
Andrei <andreialitvinenco@gmail.com>
2023-12-22 13:28:50 +0200

    Updated the file so it uses the Page Object Model.

--- login-test.py
+++ login-test.py
@@ -3,28 +3,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from assertpy import soft_assertions, assert_that
-
-driver = webdriver.Chrome()
-driver.get("https://practicetestautomation.com/practice-test-login/")
-
-wait = WebDriverWait(driver, 10)
-
-username = driver.find_element(By.ID, "username")
-password = driver.find_element(By.ID, "password")
-submitButton = driver.find_element(By.ID, "submit")
-
-username.send_keys("student")
-password.send_keys("Password123")
-submitButton.click()
-
-title = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "h1.post-title")))
-
-#driver.find_element(By.CSS_SELECTOR, "h1.post-title")
+import unittest
 
 
+class LoginPage(unittest.TestCase):
 
-with soft_assertions():
-    assert_that(title).is_true()
+    def setUp(self):
+       self.driver = webdriver.Chrome()
+       self.driver.get("https://practicetestautomation.com/practice-test-login/")
 
-driver.quit()
+    def test_login(self):
+        wait = WebDriverWait(self.driver, 10)
+
+        username = self.driver.find_element(By.ID, "username")
+        password = self.driver.find_element(By.ID, "password")
+        submitButton = self.driver.find_element(By.ID, "submit")
+
+        username.send_keys("student")
+        password.send_keys("Password123")
+        submitButton.click()
+
+        title = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "h1.post-title")))
+
+        #driver.find_element(By.CSS_SELECTOR, "h1.post-title")
+
+        with soft_assertions():
+            assert_that(title).is_true()
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
     
